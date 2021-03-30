fe7a70da45b50d8cbfd603d846915efe8168d339
ikrimahalwahidi <ikrimah.wahidi@gmail.com>
2021-03-30 16:04:04 +0300

    new201

--- js/app.js
+++ js/app.js
@@ -1,57 +1,57 @@
 'use strict'
 
-alert('hello student');
-
-let feeling = confirm('are you understand lecture');
-if (feeling === true) {
-    console.log('thats great');
-} else {
-    console.log('i will repeat');
-
-}
-
-let favouriteTeacher = prompt('who is the best teacher?');
-favouriteTeacher = favouriteTeacher.toLowerCase();
-console.log(favouriteTeacher);
 
 
-let favouriteSubject = prompt('what do you like Arabic || english?');
-favouriteSubject = favouriteSubject.toUpperCase();
-console.log(favouriteSubject);
+// let feeling = confirm('are you understand lecture');
+// if (feeling === true) {
+//     console.log('thats great');
+// } else {
+//     console.log('i will repeat');
+
+// }
+
+// let favouriteTeacher = prompt('who is the best teacher?');
+// favouriteTeacher = favouriteTeacher.toLowerCase();
+// console.log(favouriteTeacher);
+
+
+// let favouriteSubject = prompt('what do you like Arabic || english?');
+// favouriteSubject = favouriteSubject.toUpperCase();
+// console.log(favouriteSubject);
 
 
 
-let major = confirm('do you like a software');
-if (major === true) {
-    console.log('thats great');
-} else {
-    console.log('why your here?');
+// let major = confirm('do you like a software');
+// if (major === true) {
+//     console.log('thats great');
+// } else {
+//     console.log('why your here?');
 
-}
+// }
 
-let goal = confirm('are you sure to be in 401');
-if (goal === true) {
-    console.log('i hope so');
-} else {
-    console.log('do the best');
+// let goal = confirm('are you sure to be in 401');
+// if (goal === true) {
+//     console.log('i hope so');
+// } else {
+//     console.log('do the best');
 
-}
+// }
 
-let bmw = confirm('is bmw  fastest car');
-if (bmw === true) {
-    console.log('great');
-} else {
-    console.log('search about it');
+// let bmw = confirm('is bmw  fastest car');
+// if (bmw === true) {
+//     console.log('great');
+// } else {
+//     console.log('search about it');
 
-}
+// }
 
-let time = confirm('are you prefere 9 am to take a lecture ?');
-if (time === true) {
-    console.log('very early');
-} else {
-    console.log('thats great');
+// let time = confirm('are you prefere 9 am to take a lecture ?');
+// if (time === true) {
+//     console.log('very early');
+// } else {
+//     console.log('thats great');
 
-}
+// }
 
 // let time = prompt('are you prefere 9 am to take a lecture ?');
 
@@ -69,3 +69,127 @@ if (time === true) {
 //         console.log('sorry we cant');
 //         break;
 // }
+
+// let time = prompt('are you prefere 9 am to take a lecture ?');
+
+
+
+// var name=prompt("is it my name ikrimah?");
+
+// if (name=="yes"){
+
+// alert("That’s right");
+
+// }else if (name=='no') {
+// alert("That’s wrong");
+
+// }
+// var age=prompt("is it 30 years old?");
+
+// if (age=="yes"){
+
+// alert("That’s right");
+
+// }else if (age=='no') {
+// alert("That’s wrong");
+
+// }
+// var placeOfBirth=prompt("is born in amman?");
+
+// if (placeOfBirth=="yes"){
+
+// alert("That’s right");
+
+// }else if (placeOfBirth=='no') {
+// alert("That’s wrong");
+
+// }
+// var marital=prompt("is it single?");
+
+// if (marital=="yes"){
+
+// alert("That’s right");
+
+// }else if (marital=='no') {
+// alert("That’s wrong");
+
+// }
+// var fruite=prompt("are you like a mango?");
+
+// if (fruite=="yes"){
+
+// alert("good");
+
+// }else if (fruite=='no') {
+// alert("its a Delicious");
+
+// }
+// let time = prompt('are you prefere 9 am to take a lecture ?');
+
+// switch (time.toLowerCase()) {
+//     case '9 am':
+//         console.log('very early');
+//         break;
+//     case '11 am':
+//     case '12 am':
+//         console.log('thats good ');
+//         break;
+
+//     default:
+//         console.log('sorry we cant');
+//         break;
+// }
+let userName=prompt("what's your name please?");
+alert('welcome to my page '+ userName);
+confirm('please answer the question below')
+
+alert('please answer with only yes/y or no/n')
+
+
+let fruite=prompt("do i like a mango?");
+fruite=fruite.toLowerCase();
+if (fruite==='yes'|| fruite === 'y'){
+
+alert("right i like it much");
+
+}else if (fruite==='no'|| fruite === 'n') {
+alert("try it its a Delicious");
+
+}
+
+
+let time=prompt("do i prefere 9 am to take a lecture ?");
+time=time.toLowerCase();
+if (time==='yes'|| time === 'y'){
+
+alert("very early");
+
+}else if (time==='no'|| time === 'n') {
+alert("that's much good");
+
+}
+
+
+let placeOfBirth=prompt("are i born in amman ?");
+placeOfBirth=placeOfBirth.toLowerCase();
+if (placeOfBirth==='yes'|| placeOfBirth === 'y'){
+
+alert("that's right");
+
+}else if (placeOfBirth==='no'|| placeOfBirth === 'n') {
+alert("that's wrong");
+
+}
+
+let travel=prompt("doi like a travel ?");
+travel=travel.toLowerCase();
+if (travel==='yes'|| travel === 'y'){
+
+alert("that's right");
+
+}else if (travel==='no'|| travel === 'n') {
+alert("you should to try traviling");
+
+}
+
+alert(`thank you  and see soon ${userName}`);
